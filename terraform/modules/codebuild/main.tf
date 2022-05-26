92e5c0d6bcb337e5eb90ffd3cb7a28804d80ed9e
fckurethn <fckurethn@gmail.com>
2022-05-26 18:26:16 +0300

    correct build project description

--- terraform/modules/codebuild/main.tf
+++ terraform/modules/codebuild/main.tf
@@ -32,7 +32,7 @@ resource "aws_codebuild_source_credential" "this" {
 
 resource "aws_codebuild_project" "demo-pepe" {
   name          = "${var.env}-pepe"
-  description   = "test_codebuild_project"
+  description   = "${var.env}_codebuild_project"
   build_timeout = "5"
   service_role  = aws_iam_role.codebuild_role.arn
 
